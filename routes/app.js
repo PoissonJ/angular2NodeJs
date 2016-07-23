2acb6845eac2757a58b41bbe09d13551ae9734a8
Jonathan <poisson.jonathan777@gmail.com>
2016-07-22 20:35:08 -0400

    Worked with displaying and entering data into mongodb using mongoose

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-router.get('/:userTitle?', function (req, res, next) {
-    res.render('node', {title: req.params.userTitle ? req.params.userTitle : ''});
+router.get('/', function(req, res, next) {
+  var email = ''
+  User.findOne({}, function(err, doc) {
+    if (err) {
+      return res.send('Error');
+    }
+    if (doc) {
+      email = doc.email // doc is the user
+    }
+    res.render('node', {email: email});
+  });
 });
 
-router.post('/', function (req, res, next) {
-    var title = req.body.title;
-    res.redirect('/' + title);
+router.post('/', function(req, res, next) {
+  var title = req.body.email;
+  var user = new User({
+    firstName: "Jonathan",
+    lastName: "Poisson",
+    password: "test",
+    email: "test@test.com",
+  });
+  user.save();
+  res.redirect('/');
 });
 
 module.exports = router;
