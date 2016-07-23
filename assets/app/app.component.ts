e1a66767b97e3541881b1aebfae12985c2e7d72a
Jonathan <poisson.jonathan777@gmail.com>
2016-07-22 23:33:33 -0400

    Added styles to home page

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    templateUrl: 'app.template.html'
+    templateUrl: 'app.template.html',
+    styles: [ `
+      .author {
+        display: inline-block;
+        font-style: italic;
+        font-size: 12px;
+        width: 80%;
+      }
+      .config {
+        display: inline-block;
+        text-align: right;
+        font-size: 12px;
+        width: 19%;
+      }
+    `]
     // template: `
     //     <h1>Hello World</h1>
     // `
