b0298463711a3da6bfac839d8484c8b54c88cb82
Jonathan <poisson.jonathan777@gmail.com>
2016-07-22 22:50:11 -0400

    Removed node routes to prepare for angular2

--- routes/app.js
+++ routes/app.js
@@ -3,28 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function(req, res, next) {
-  var email = ''
-  User.findOne({}, function(err, doc) {
-    if (err) {
-      return res.send('Error');
-    }
-    if (doc) {
-      email = doc.email // doc is the user
-    }
-    res.render('node', {email: email});
-  });
-});
-
-router.post('/', function(req, res, next) {
-  var title = req.body.email;
-  var user = new User({
-    firstName: "Jonathan",
-    lastName: "Poisson",
-    password: "test",
-    email: "test@test.com"
-  });
-  user.save();
-  res.redirect('/');
+  res.render('index');
 });
 
 module.exports = router;
