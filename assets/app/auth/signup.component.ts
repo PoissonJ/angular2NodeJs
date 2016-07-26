ef421fe8ff140bcf4a1371ad252fdbeee8ccaca4
Jonathan <poisson.jonathan777@gmail.com>
2016-07-26 19:08:03 -0400

    Added custom validation to email on signup form

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {FormBuilder, ControlGroup, Validators} from "@angular/common";
+import {FormBuilder, ControlGroup, Validators, Control} from "@angular/common";
 @Component({
   moduleId: module.id, // necessary when using tsconfig "commonjs" for file path resolution
   selector: 'my-signup',
@@ -18,8 +18,17 @@ export class SignupComponent implements OnInit {
     this.myForm = this._fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, this.isEmail])],
       password: ['', Validators.required]
     });
+  }
+
+  // Only retuns a value if the validation fails
+  private isEmail(control: Control): { [s: string]: boolean } {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!control.value.match(re)) {
+      return { invalidMail: true };
+    }
   }
 }
